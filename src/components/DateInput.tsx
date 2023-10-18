ace065f0499a7dc198242cb29257e66d6aafeb80
Linus Lehbrink <hi@linus.systems>
2023-10-18 04:20:29 +0200

    Base

---
+++ src/components/DateInput.tsx
@@ -0,0 +1,122 @@
+import clsx from "clsx";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+interface Props {
+  setDate: (date: Date | null) => void;
+}
+
+const DateInput = ({
+  setDate,
+  ...props
+}: Props & React.ComponentPropsWithoutRef<"div">) => {
+  const [error, setError] = useState<boolean>(false);
+  const [digits, setDigits] = useState<(string | null)[]>(Array(8).fill(null));
+  const [focusedIndex, setFocusedIndex] = useState<number | null>(-1);
+  const refs = Array.from({ length: 8 }, () => useRef<HTMLInputElement>(null));
+  const placeholders = ["D", "D", "M", "M", "Y", "Y", "Y", "Y"];
+
+  useEffect(() => {
+    const [d1, d2, m1, m2, y1, y2, y3, y4] = digits;
+    const day = parseInt(`${d1}${d2}`, 10);
+    const month = parseInt(`${m1}${m2}`, 10) - 1;
+    const year = parseInt(`${y1}${y2}${y3}${y4}`, 10);
+    const date = new Date(year, month, day);
+
+    if (digits.every((d) => d !== null)) {
+      if (isNaN(date.getTime())) {
+        setError(true);
+      }
+      if (date > new Date()) {
+        setError(true);
+      }
+    } else {
+      setDate(null);
+    }
+
+    if (
+      digits.every((d) => d !== null) &&
+      !isNaN(date.getTime()) &&
+      date < new Date()
+    ) {
+      setDate(date);
+      refs[refs.length - 1].current?.blur();
+      setError(false);
+    }
+  }, [digits]);
+
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    e.preventDefault();
+    const value = e.key;
+
+    if (/^[0-9]$/.test(value)) {
+      setDigits((prevDigits) => {
+        const newDigits = [...prevDigits];
+        newDigits[index] = value;
+        return newDigits;
+      });
+
+      if (index < refs.length - 1) {
+        const nextInput = refs[index + 1];
+        if (nextInput.current) {
+          nextInput.current.focus();
+        }
+      }
+    }
+
+    if (e.key === "Backspace") {
+      if (digits[index] !== null) {
+        setDigits((prevDigits) => {
+          const newDigits = [...prevDigits];
+          newDigits[index] = null;
+          return newDigits;
+        });
+      } else if (index > 0) {
+        setDigits((prevDigits) => {
+          const newDigits = [...prevDigits];
+          newDigits[index - 1] = null;
+          return newDigits;
+        });
+
+        const prevInput = refs[index - 1];
+        if (prevInput.current) {
+          prevInput.current.focus();
+        }
+      }
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-1" {...props}>
+      {Array.from({ length: 8 }).map((_, index) => (
+        <Fragment key={index}>
+          <input
+            key={index}
+            ref={refs[index]}
+            type="tel"
+            inputMode="numeric"
+            maxLength={1}
+            placeholder={placeholders[index]}
+            onFocus={() => setFocusedIndex(index)}
+            onBlur={() => setFocusedIndex(null)}
+            className={clsx(
+              "h-10 w-8 rounded border bg-white text-center text-base text-black caret-transparent outline-none transition-all duration-150 focus:outline-none",
+              error && digits.every((d) => d !== null) && "border-red-500",
+              focusedIndex === index || digits[index] !== null
+                ? "border-black"
+                : "border-gray-300",
+            )}
+            defaultValue={digits[index] ?? ""}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          />
+          {index === 1 || index === 3 ? <span className="w-1"></span> : null}
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default DateInput;
